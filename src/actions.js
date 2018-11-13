d860c47955ce3c58942e40745a54080087ae11f6
Rimvydas <tamosiunas.rimvydas@gmail.com>
2018-11-13 21:54:06 +0200

    initial commit no logger

--- src/actions.js
+++
@@ -1,7 +0,0 @@
-export const setMovies = (movies) => {
-  // action - always return only object with data, no functionality can be done here
-  return {
-    type: 'SET_MOVIES',
-    movies,
-  };
-};
